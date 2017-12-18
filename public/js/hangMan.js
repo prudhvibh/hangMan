0ad59ec8c4035f110707e9c74b97d38198c2e241
prudhvi <prudhvibhavanari@gmail.com>
2017-12-18 16:41:26 +0530

    Hang man game with countries names

---
+++ public/js/hangMan.js
@@ -0,0 +1,205 @@
+let words=[
+  'Afghanistan',
+  'Albania',
+  'Algeria',
+  'Andorra',
+  'Angola',
+  'Argentina',
+  'Armenia',
+  'Aruba',
+  'Australia',
+  'Austria',
+  'Azerbaijan',
+  'Bahrain',
+  'Bangladesh',
+  'Barbados',
+  'Belgium',
+  'Belize',
+  'Benin',
+  'Bhutan',
+  'Bolivia',
+  'Botswana',
+  'Brazil',
+  'Brunei',
+  'Bulgaria',
+  'Burma',
+  'Burundi',
+  'Cambodia',
+  'Cameroon',
+  'Canada',
+  'Chad',
+  'Chile',
+  'China',
+  'Colombia',
+  'Comoros',
+  'Congo',
+  'Croatia',
+  'Cuba',
+  'Curacao',
+  'Cyprus',
+  'Denmark',
+  'Djibouti',
+  'Dominica',
+  'Ecuador',
+  'Egypt',
+  'Eritrea',
+  'Estonia',
+  'Ethiopia',
+  'Fiji',
+  'Finland',
+  'France',
+  'Gabon',
+  'Georgia',
+  'Germany',
+  'Ghana',
+  'Greece',
+  'Grenada',
+  'Guatemala',
+  'Guinea',
+  'Guyana',
+  'Haiti',
+  'Honduras',
+  'Hungary',
+  'Iceland',
+  'India',
+  'Indonesia',
+  'Iran',
+  'Iraq',
+  'Ireland',
+  'Israel',
+  'Italy',
+  'Jamaica',
+  'Japan',
+  'Jordan',
+  'Kazakhstan',
+  'Kenya',
+  'Kiribati',
+  'Kosovo',
+  'Kuwait',
+  'Kyrgyzstan',
+  'Laos',
+  'Latvia',
+  'Lebanon',
+  'Lesotho',
+  'Liberia',
+  'Libya',
+  'Liechtenstein',
+  'Lithuania',
+  'Luxembourg',
+  'Macau',
+  'Macedonia',
+  'Madagascar',
+  'Malawi',
+  'Malaysia',
+  'Maldives',
+  'Mali',
+  'Malta',
+  'Mauritania',
+  'Mauritius',
+  'Mexico',
+  'Micronesia',
+  'Moldova',
+  'Monaco',
+  'Mongolia',
+  'Montenegro',
+  'Morocco',
+  'Mozambique',
+  'Namibia',
+  'Nauru',
+  'Nepal',
+  'Netherlands',
+  'Nicaragua',
+  'Niger',
+  'Nigeria',
+  'Norway',
+  'Oman',
+  'Pakistan',
+  'Palau',
+  'Panama',
+  'Paraguay',
+  'Peru',
+  'Philippines',
+  'Poland',
+  'Portugal',
+  'Qatar',
+  'Romania',
+  'Russia',
+  'Rwanda',
+  'Samoa',
+  'Senegal',
+  'Serbia',
+  'Seychelles',
+  'Singapore',
+  'Slovakia',
+  'Slovenia',
+  'Somalia',
+  'Spain',
+  'Sudan',
+  'Suriname',
+  'Swaziland',
+  'Sweden',
+  'Switzerland',
+  'Syria',
+  'Taiwan',
+  'Tajikistan',
+  'Tanzania',
+  'Thailand',
+  'Togo',
+  'Tonga',
+  'Tunisia',
+  'Turkey',
+  'Turkmenistan',
+  'Tuvalu',
+  'Uganda',
+  'Ukraine',
+  'Uruguay',
+  'Uzbekistan',
+  'Vanuatu',
+  'Venezuela',
+  'Vietnam',
+  'Yemen',
+  'Zambia',
+  'Zimbabwe'
+];
+
+let randomWord = function() {
+  let index = Math.floor(Math.random() * words.length);
+  return words[index].toLowerCase();
+}
+
+// console.log(randomWord());
+
+let emptyLetters = function(length) {
+  let output = [];
+  for(let index=0;index<length;index++){
+    output.push('_');
+  }
+  return output.join(' ');
+}
+
+// console.log(emptyLetters(5));
+
+let checkLetter = function(word,letter) {
+  let indexOfLetterInWord = [];
+  for(let index=0;index<word.length;index++){
+    if(letter == word[index]){
+      indexOfLetterInWord.push(index);
+    }
+  }
+  return indexOfLetterInWord;
+}
+
+// console.log(checkLetter("india","i"));
+
+
+let updateDisplay = function(emptyItems,indexOfLetterInWord,letter) {
+  for(let index=0;index<indexOfLetterInWord.length;index++){
+    emptyItems[indexOfLetterInWord[index]] = letter;
+  }
+  emptyItems = emptyItems.join(' ');
+  document.getElementById('input').innerText = emptyItems;
+  return emptyItems;
+}
+
+
+// console.log(updateDisplay(['_','_','_','_'],[1,3],'i'));
---
+++ public/js/hangManController.js
@@ -0,0 +1,67 @@
+let score = 0;
+let wrongMoves = [];
+let images = ['hang_1.gif','hang_2.gif','hang_3.gif','hang_4.gif','hang_5.gif','hang_6.gif','hang_7.gif'];
+let startGame = function() {
+  let word = randomWord();
+  let dashes = emptyLetters(word.length);
+  document.getElementById('input').innerText = dashes;
+  game(word,dashes);
+  wrongMoves = [];
+  document.getElementById('wrongKeys').innerHTML = [];
+}
+
+let isGameWon = function(word,dashes) {
+  return dashes.toString()==word.split('').toString();
+}
+
+let isGameLost = function(wrongMoves,word) {
+  changeImage(wrongMoves.length);
+  if(wrongMoves.length >= 6) {
+    alert('You loss the game'+'\n'+'Correct word is '+word);
+    location.reload();
+  }
+}
+
+let scoreOfGame = function() {
+  score++;
+  document.getElementById('score').innerText = "Score:"+score;
+  startGame();
+}
+
+let printWrongMove = function(wrongMoves,letter) {
+  if(!wrongMoves.includes(letter)){
+    wrongMoves.push(letter);
+    document.getElementById('wrongKeys').innerHTML =
+    wrongMoves.join(' ');
+  }
+}
+
+let changeImage = function(index) {
+  let image = document.getElementById('img');
+  image.src = 'public/images/'+images[index];
+}
+
+let game = function(word,dashes) {
+  dashes = dashes.split(' ');
+  window.onkeypress = function(event){
+    let letter = event.key;
+    let indexOfLetterInWord=checkLetter(word,letter);
+    if(indexOfLetterInWord.length>0){
+      updateDisplay(dashes,indexOfLetterInWord,letter);
+      if(isGameWon(word,dashes)){
+        scoreOfGame();
+        changeImage(0);
+      }
+    } else {
+      printWrongMove(wrongMoves,letter);
+    }
+    isGameLost(wrongMoves,word);
+  }
+}
+
+window.onload = startGame;
+
+//if you want to use onclick replace with this.
+// let buttons = document.getElementById('buttonGroup');
+// buttons.onclick = function(event){
+//   let letter = event.target.id;
